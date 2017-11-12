a2a7abc434508f0271c8c8b67f3f9b83a0a7b134
ShotaAk <macakasit@gmail.com>
2017-11-12 23:03:57 +0900

    Change Sequence node to Sequence with memory node

--- pi_trees_lib/src/pi_trees_lib/pi_trees_lib.py
+++ pi_trees_lib/src/pi_trees_lib/pi_trees_lib.py
@@ -161,6 +161,9 @@ class Sequence(Task):
             
         for c in self.children:
             
+            if c.status == TaskStatus.SUCCESS:
+                continue
+
             c.status = c.run()
                          
             if c.status != TaskStatus.SUCCESS:
